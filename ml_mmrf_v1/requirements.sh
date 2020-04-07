4e5150e4b558d80b118eb6321a721723a994e79f
Rahul G. Krishnan <rahulk90@gmail.com>
2020-04-07 14:08:51 -0400

    updated requirements

---
+++ ml_mmrf_v1/requirements.sh
@@ -0,0 +1,6 @@
+conda create -n data_v1 python=3.7.3
+conda activate data_v1
+conda install matplotlib scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
+conda install -c conda-forge tslearn ipdb
+pip install pyro-ppl tensorflow_probability jupyter-tensorboard
+conda install -c conda-forge ncurses
