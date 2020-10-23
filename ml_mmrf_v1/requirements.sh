64ec7591b8f075262140b131304eeb339832eda7
Your Name <you@example.com>
2020-10-23 15:23:32 -0400

    v1 reqs

--- ml_mmrf_v1/requirements.sh
+++ ml_mmrf_v1/requirements.sh
@@ -2,7 +2,7 @@ conda create -n data_v1 python=3.7.3
 conda activate data_v1
 conda install matplotlib scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
 conda install -c conda-forge tslearn ipdb
-pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines
+pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines scanpy
 conda install -c brittainhard fancyimpute
 conda install -c conda-forge ncurses
 conda install pytorch cudatoolkit=10.1 torchvision -c pytorch 
