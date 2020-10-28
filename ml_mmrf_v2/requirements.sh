2bf6276bbce9803ea48a4a89a5452ce4dc6a9c85
Rebecca Peyser <Rebecca@MacBook-Air.local>
2020-10-28 17:52:56 -0400

    add scanpy[leiden] for full capabilities

--- ml_mmrf_v2/requirements.sh
+++ ml_mmrf_v2/requirements.sh
@@ -1,8 +1,8 @@
-conda create -n data_v1 python=3.7.3
-conda activate data_v1
-conda install matplotlib scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
+conda create -n data_v2 python=3.7.3
+conda activate data_v2
+conda install matplotlib scanpy scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
 conda install -c conda-forge tslearn ipdb
-pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines scanpy
+pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines scanpy[leiden]
 conda install -c brittainhard fancyimpute
 conda install -c conda-forge ncurses
-conda install pytorch cudatoolkit=10.1 torchvision -c pytorch 
+conda install pytorch cudatoolkit=10.1 torchvision -c pytorch
