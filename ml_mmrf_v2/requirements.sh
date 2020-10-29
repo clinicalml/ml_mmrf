1d1ea40dff7cd47e8e8676f8f9e99f9b86d00ab3
Rebecca Peyser <Rebecca@MacBook-Air.local>
2020-10-28 23:08:08 -0400

    anndata in scanpy is buggy - upgrade

--- ml_mmrf_v2/requirements.sh
+++ ml_mmrf_v2/requirements.sh
@@ -3,6 +3,7 @@ conda activate data_v2
 conda install matplotlib scanpy scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
 conda install -c conda-forge tslearn ipdb
 pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines scanpy[leiden]
+pip install anndata -U
 conda install -c brittainhard fancyimpute
 conda install -c conda-forge ncurses
 conda install pytorch cudatoolkit=10.1 torchvision -c pytorch
