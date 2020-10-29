8ab30acd22fe8a1eabb5a4c5d23ef559a6773b6b
Rebecca Peyser <Rebecca@MacBook-Air.local>
2020-10-28 23:09:25 -0400

    update requirements.sh

--- ml_mmrf_v2/requirements.sh
+++ ml_mmrf_v2/requirements.sh
@@ -2,7 +2,8 @@ conda create -n data_v2 python=3.7.3
 conda activate data_v2
 conda install matplotlib scanpy scipy numpy pandas h5py ipython jupyterlab cython seaborn scikit-learn r
 conda install -c conda-forge tslearn ipdb
-pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines scanpy[leiden]
+pip install pyro-ppl tensorflow_probability jupyter-tensorboard lifelines
+pip install scanpy[leiden]
 pip install anndata -U
 conda install -c brittainhard fancyimpute
 conda install -c conda-forge ncurses
